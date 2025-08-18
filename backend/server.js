0c9bac98298ade626db3fdc7c30f91e789afa542
Suchitra Verma <thisissuchitraverma@gmail.com>
2025-08-18 18:56:19 +0530

    cartRoute Spelling fix

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/Cloudinary.js'
 import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
-import cartRouter from './routes/CartRoute.js'
+import cartRouter from './routes/cartRoute.js'
 import orderRouter from './routes/orderRoute.js'
 import multer from 'multer';
 import path from 'path';
